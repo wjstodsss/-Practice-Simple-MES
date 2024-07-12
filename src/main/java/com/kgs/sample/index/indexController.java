2d833262c509d82f969d0326c039faa7d3b00a68
finn <wjstodsss@naver.com>
2024-07-12 19:24:54 +0900

    first commit

---
+++ src/main/java/com/kgs/sample/index/indexController.java
@@ -0,0 +1,12 @@
+package com.kgs.sample.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    @RequestMapping( "/" )
+    public String index() { 
+    	return "index"; 
+    }
+}
